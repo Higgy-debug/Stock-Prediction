a849dfc898dd62da275e3d1eb856a6fa28f8f9d0
Hirthickkesh <74501685+Higgy-debug@users.noreply.github.com>
2021-02-22 03:10:09 +0530

    Create bulit and train model.py

---
+++ bulit and train model.py
@@ -0,0 +1,10 @@
+lstm_model=Sequential()
+lstm_model.add(LSTM(units=50,return_sequences=True,input_shape=(x_train_data.shape[1],1)))
+lstm_model.add(LSTM(units=50))
+lstm_model.add(Dense(1))
+
+
+
+
+lstm_model.compile(loss='mean_squared_error',optimizer='adam')
+lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
